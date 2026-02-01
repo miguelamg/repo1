99b767be03c886bb97b08aeef004292a7c0a2765
migue <migue@192.168.1.42>
2026-02-01 20:46:57 +0100

    Variable Scanner.

---
+++ Ej1_EsPar/src/ejecuta/Inicio.java
@@ -0,0 +1,16 @@
+package ejecuta;
+
+import java.util.Scanner;
+
+public class Inicio 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc;
+		System.out.println("Algoritmo para indicar si un número es par");
+		
+
+	}
+
+}
